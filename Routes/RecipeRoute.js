b6a621346b8ebd111f8f88a0e7dd0072839aba01
DefnotCash <kcash1316@gmail.com>
2022-09-26 20:34:16 +0100

    Recent Change has been giving error

--- Routes/RecipeRoute.js
+++ Routes/RecipeRoute.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import { createRecipe, deleteRecipe, updateRecipe, getAllRecipe, getRecipe } from '../controllers/RecipeController.js';
+import { adminUser, prof } from '../middleware/admin.js';
+import verifyToken from '../middleware/authmiddle.js';
 
 const router = express.Router();
 
 router.get('/', getAllRecipe);
-router.get('/recipe', getRecipe);
-router.post('/create', createRecipe);
-router.put('/update/:id', updateRecipe);
-router.delete('/delete/:id', deleteRecipe);
+router.get('/:id', verifyToken, getRecipe);
+router.post('/create', verifyToken, prof, createRecipe);
+router.put('/update/:id',verifyToken,adminUser, updateRecipe);
+router.delete('/delete/:id',verifyToken,adminUser, deleteRecipe);
 
 export default router;
+
+
